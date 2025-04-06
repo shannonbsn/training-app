498595aeafae41e41d176dd4a1b66f255a1cfb58
Shannon Besson <shannon0413@outlook.com>
2025-04-06 18:26:43 +0200

    (feat(ExcerciceScreen): Ajout des cases en numeric)

--- app/ExerciceScreen.tsx
+++ app/ExerciceScreen.tsx
@@ -1,18 +1,27 @@
 import { Text, View, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
 import { Link } from 'expo-router';
 
 export default function Index() {
+    const [time, setTime] = useState("");
+    const [repetition, setRepetition] = useState("");
+    const [breakTime, setBreakTime] = useState("");
+
+    const handleNumericInput = (text: string, setter: React.Dispatch<React.SetStateAction<string>>) => {
+        const numericValue = text.replace(/[^0-9]/g, "");
+        setter(numericValue);
+    };
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Exercice</Text>
             <Text style={styles.label}>Name</Text>
             <TextInput style={styles.input} placeholder="Squat" placeholderTextColor="#E0F2F1" />
             <Text style={styles.label}>Time</Text>
-            <TextInput style={styles.input} placeholder="30" placeholderTextColor="#E0F2F1" />
+            <TextInput style={styles.input} placeholder="30" placeholderTextColor="#E0F2F1" keyboardType="numeric" value={time} onChangeText={(text) => handleNumericInput(text, setTime)} />
             <Text style={styles.label}>Repetition</Text>
-            <TextInput style={styles.input} placeholder="5" placeholderTextColor="#E0F2F1" />
+            <TextInput style={styles.input} placeholder="5" placeholderTextColor="#E0F2F1" keyboardType="numeric" value={repetition} onChangeText={(text) => handleNumericInput(text, setRepetition)} />
             <Text style={styles.label}>Break</Text>
-            <TextInput style={styles.input} placeholder="30" placeholderTextColor="#E0F2F1" />
+            <TextInput style={styles.input} placeholder="30" placeholderTextColor="#E0F2F1" keyboardType="numeric" value={breakTime} onChangeText={(text) => handleNumericInput(text, setBreakTime)} />
             <Link href="/ExerciceScreen" style={styles.button}>
                 Add
             </Link>
